8cc4e87f0c7519327a546ea305332e6ab016404d
faiyaz032 <faiyazrahman03@gmail.com>
2024-10-12 23:51:22 +0600

    Replace pino logger with winston

--- src/shared/logger/LoggerManager.ts
+++ src/shared/logger/LoggerManager.ts
@@ -1,21 +1,36 @@
 import dayjs from 'dayjs';
-import pino from 'pino';
+import winston from 'winston';
 
 class LoggerManager {
   static createLogger() {
-    const logger = pino({
-      base: {
-        pid: false,
-      },
+    const customFormat = winston.format.printf(({ level, message, timestamp }) => {
+      return `${timestamp} ${level}: ${message}`;
+    });
 
-      transport: {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          ignore: 'pid,hostname',
-          translateTime: `${dayjs().format('YYYY-MM-DD HH:mm:ss')}`,
-        },
-      },
+    const logger = winston.createLogger({
+      level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+      format: winston.format.combine(
+        winston.format.colorize({ all: true }),
+        winston.format.timestamp({ format: () => dayjs().format('YYYY-MM-DD HH:mm:ss') }),
+        customFormat
+      ),
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.colorize({
+              all: true,
+              colors: {
+                info: 'green',
+                error: 'red',
+                warn: 'yellow',
+                debug: 'blue',
+              },
+            }),
+            customFormat
+          ),
+        }),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      ],
     });
 
     return logger;
